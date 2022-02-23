ddcb949c897c6b007a2e65752e086b9472fd5d7e
= <=>
2022-02-23 13:57:05 -0500

    done2

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 900);
   }
 
   /** 
@@ -25,11 +25,58 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    //Drawing grass (rectangle)
+    fill(100,225,100);
+    stroke(0);
+    rect(0, (float) (height*0.6), (width), (height));
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //Drawing first house (base - rectangle) (roof - triangle)
+    fill(129, 128, 128);
+    stroke(4);
+    rect((float) (width/4), (float) (height/2.5), (float) (width/2), (float) (height/2.5));
+    
+    fill(110, 110, 110);
+    stroke(0);
+    triangle((float) (width/4), (float) (height/2.5), (float) (width/2), (float) (height/5), (float) (width*0.75), (float) (height/2.5));
+    
+    fill(255, 100, 100);
+    stroke(200);
+    rect((float) (width*0.3125), (float) (height*0.6), (float) (width/8), (float) (height/5));
+
+    //Drawing second house (base - rectangle) (roof - triangle)
+    fill(110, 110, 110);
+    stroke(0);
+    triangle((float) (width*0.8125), (float) (height*0.58), (float) (width*0.875), (float) (height*0.54), (float) (width*0.9375), (float) (height*0.58));
+
+
+    fill(128, 128, 128);
+    stroke(0);
+    rect((float) (width*0.8125), (float) (height*0.58), (float) (width/8), (float) (height*0.15));
+    
+    fill(255, 100, 100);
+    stroke(200);
+    rect((float) (width*0.83), (float) (height*0.63), (float) (width/20), (float) (height*0.096));
+
+    //Drawing sun (circle)
+    fill(255, 255, 0);
+    stroke(4);
+    ellipse((width), 0, (float) (width/4), (float) (height/5));
+
+
+    //Drawing sign (pole - line) (sign body - rectangle)
+    stroke(0);
+    line((float) (width/8), (float) (height*0.92), (float) (width/8), (float) (height*0.7));
+
+    
+    fill(100, 200, 200);
+    stroke(0);
+    rect((float) (width/16), (float) (height*0.73), (float) (width/8), (float) (height/20));
+
+
+    //Drawing window (rectangle)
+    fill(225, 225, 225);
+    rect((float) (width*0.475), (float) (height*0.49), (float) (width/4), (float) (height/10));
+
   }
   
   // define other methods down here.
